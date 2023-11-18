c477191876d44ba9c2981780d415503d3d753bf6
Iliyan <alibhai@MacBook-Pro-11.local>
2023-11-18 13:13:30 -0800

    accomplished button and main screen

---
+++ main.py
@@ -0,0 +1,70 @@
+import pygame
+import sys
+
+# Initialize pygame
+pygame.init()
+
+# Set up the screen
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Cutie Clean Up")  # Game name
+clock = pygame.time.Clock()
+
+# Define colors
+white = (255, 255, 255)
+orange = (255, 165, 0)
+blue = (0, 0, 255)
+azureBlue = (0, 127, 255)
+green = (0, 255, 0)
+dark_green = (0, 200, 0)
+
+# Create a font object
+font = pygame.font.SysFont('comicsansms', 30)
+text = font.render('Cutie Clean-up', True, orange, blue)
+textRect = text.get_rect(center=(width // 2, height // 3))
+
+# Define button dimensions and positions
+button_width = 140
+button_height = 40
+button_x = (width // 2) - (button_width // 2)
+button_y = height // 2
+
+# Create a font for the button text
+button_font = pygame.font.SysFont('Corbel', 35)
+button_text = button_font.render('Play', True, white)
+button_text_rect = button_text.get_rect(center=(button_x + button_width // 2, button_y + button_height // 2))
+
+running = True
+while running:
+    screen.fill(azureBlue)  # Fill the screen with azure blue
+
+    # Draw the game title text
+    screen.blit(text, textRect)
+
+    # Get mouse position and check for clicks
+    mouse_pos = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    # Draw the button
+    if button_x <= mouse_pos[0] <= button_x + button_width and button_y <= mouse_pos[1] <= button_y + button_height:
+        pygame.draw.rect(screen, green, (button_x, button_y, button_width, button_height))
+    else:
+        pygame.draw.rect(screen, dark_green, (button_x, button_y, button_width, button_height))
+
+    # Show button text
+    screen.blit(button_text, button_text_rect)
+
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if button_x <= mouse_pos[0] <= button_x + button_width and button_y <= mouse_pos[1] <= button_y + button_height:
+                print("Starting the game...")  # Replace with your game logic
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
